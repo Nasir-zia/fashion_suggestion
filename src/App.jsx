82135f1dd1c57fd5198aeb21906649e9206d2135
Abdullah Afzal <abdullah941685@gmail.com>
2025-12-17 23:02:38 +0500

    supabase added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
+import { supabase } from "./lib/supabase";
 
 function App() {
-
+  console.log("supabase");
+  console.log(supabase);
   return (
-
-<div>
-  <h1 className=''>Hello Madar fackers </h1>
-</div>
-  )
+    <div className="flex justify-content-around bg-black">
+      <h1 className=""> Hello </h1>
+      <p className="text-black">ajsdflajsdfljasldfjalsdjfaklsdjflkasjdfj</p>
+    </div>
+  );
 }
 
-export default App
+export default App;

1b81bdd4bc05826724fb62a23d305de2fb76bb38
Abdullah Afzal <abdullah941685@gmail.com>
2025-12-18 08:49:12 +0500

    seprated backend

--- Backend/utils/extractor.js
+++
@@ -1,7 +0,0 @@
-// Placeholder: aap real image processing + AI tag extraction laga sakte ho
-export const extractColorsAndTags = async (imageUrl) => {
-  return {
-    colors: ["red", "white", "blue"],
-    tags: ["casual", "shirt", "formal"]
-  };
-};
